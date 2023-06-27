c81355012623130b4f06ce127649728d3913418c
SparkerXof <sparket15@gmail.com>
2023-06-27 19:11:53 +0300

    Added Projectile shooting

--- Assets/CombatScript.cs
+++ Assets/CombatScript.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class CombatScript : MonoBehaviour
 {
+    public GameObject projectile;
+    public Transform aimCamera;
+    public Transform spawner;
+    public float projectileSpeed;
+    public float projectileDamage;
     public float shootDelay;
+    private float shootDelayTimer;
+    
     void Update()
     {
         if (Input.GetButton("Shoot"))
         {
-
+            if (shootDelayTimer < 0)
+            {
+                projectile.GetComponent<PlayerProjectileScript>().baseSpeed = projectileSpeed;
+                projectile.GetComponent<PlayerProjectileScript>().damage = projectileDamage;
+                Instantiate(projectile, spawner.position, aimCamera.rotation);
+                shootDelayTimer = shootDelay;
+            }
+            shootDelayTimer -= Time.deltaTime;
         }
     }
 }
---
+++ Assets/PlayerProjectileScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerProjectileScript : MonoBehaviour
+{
+    public float baseSpeed;
+    public float damage;
+    private Rigidbody rb;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Enemy")
+        {
+            
+        }
+        Destroy(gameObject);
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddRelativeForce(Vector3.forward * baseSpeed, ForceMode.Impulse);
+    }
+
+    void Update()
+    {
+
+    }
+}
